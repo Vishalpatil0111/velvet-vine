84ca5267a195030241ccc22c6fc8b875e496ba05
Vishalpatil0111 <vp4137841@gmail.com>
2026-04-26 20:00:42 +0530

    Logo and name

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
-import { createRouter } from "@tanstack/react-router";
+import {
+  createStartHandler,
+  defaultStreamHandler,
+} from "@tanstack/react-start/server";
 
-export async function handleRequest(request: Request) {
-  // Get the pathname from the request URL
-  const url = new URL(request.url);
-  const pathname = url.pathname;
-
-  // Don't rewrite API calls or static assets
-  if (pathname.startsWith("/api/") || /\.[^/]+$/.test(pathname)) {
-    return null; // Let the default handler take over
-  }
-
-  // For all other routes, this will be handled by the SPA router
-  return null;
-}
+export default {
+  fetch: createStartHandler(defaultStreamHandler),
+};
